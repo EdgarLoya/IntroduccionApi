06f4f22f6171ef40b9692b08a0db9e8ea2338d19
EdgarLoya <57698330+EdgarLoya@users.noreply.github.com>
2022-05-10 11:53:40 -0600

    modificando el endponit abriendo para abrir 2 archvos

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/hola', function(req, res){
     res.send('Hola Mundo');
+});
+app.get('/adios', function(req, res){
+    res.send('Adios Mundo');
 });
 app.listen(3000, function(){
     console.log('Aplicacion ejempo, escuchando en el puerto 3000!');
