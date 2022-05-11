f686e4a6ba930d9571756fcda45c7866a425c1ad
EdgarLoya <57698330+EdgarLoya@users.noreply.github.com>
2022-05-11 11:45:16 -0600

    commit para la creacion de la ruta para los usuarios

--- routes/routes.js
+++ routes/routes.js
@@ -6,4 +6,11 @@ const routes = app => {
             message: 'Bienvenido a Node js Exrpess REST API!!!'
         });
     });
+    app.get('/users', (request, response) =>{
+        pool.query('SELECT * FROM users', (error, result) => {
+            if(error) throw error;
+
+            response.send(result);
+        });
+    });
 }
