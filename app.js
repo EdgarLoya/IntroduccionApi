e9bec489e1286e44e710eac95f07198b76dda6b3
EdgarLoya <57698330+EdgarLoya@users.noreply.github.com>
2022-05-11 11:11:34 -0600

    commit del nucleo de nuestro servidor

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const port = 3002;
 
-app.get('/hola', function(req, res){
-    res.send('Hola Mundo');
-});
-app.get('/adios', function(req, res){
-    res.send('Adios Mundo');
-});
-app.listen(3000, function(){
-    console.log('Aplicacion ejempo, escuchando en el puerto 3000!');
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+routes(app);
+
+const server = app.listen(port, (error) =>{
+    if(error) return console.log(`Error: ${error}`);
+    console.log(`El servidor escucha en el puerto ${server.address().port}`);
 });
